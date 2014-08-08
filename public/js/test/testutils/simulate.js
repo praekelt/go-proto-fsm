e14759bd30430a33c20f61e70a2fa88e70ba2122
justinvdm <justinvdm.0.1.0@gmail.com>
2014-08-08 15:19:05 +0200

    Add d3 event simulation

--- public/js/test/testutils/simulate.js
+++ public/js/test/testutils/simulate.js
@@ -1,3 +1,85 @@
 describe('simulate', function () {
+    var el;
+
     beforeEach(module('vumigo.testutils'));
+
+    beforeEach(inject(function(simulate) {
+        el = d3.select('body').append('div');
+        simulate.enable();
+    }));
+
+    afterEach(inject(function(simulate) {
+        el.remove();
+        simulate.disable();
+    }));
+
+    it('should invoke the d3 event listeners', function() {
+        var bars = [];
+
+        el.selectAll('.foo')
+          .data([1, 2, 3, 4])
+          .enter().append('div')
+              .attr('class', 'foo')
+              .attr('data-id', function(d) { return d; })
+              .on('bar', function(d, i) {
+                  bars.push({
+                      i: i,
+                      d: d,
+                      ctx: this
+                  });
+              })
+              .simulate('bar');
+
+        expect(bars).to.deep.equal([{
+            i: 0,
+            d: 1,
+            ctx: el.select('.foo[data-id="1"]').node()
+        }, {
+            i: 1,
+            d: 2,
+            ctx: el.select('.foo[data-id="2"]').node()
+        }, {
+            i: 2,
+            d: 3,
+            ctx: el.select('.foo[data-id="3"]').node()
+        }, {
+            i: 3,
+            d: 4,
+            ctx: el.select('.foo[data-id="4"]').node()
+        }]);
+    });
+
+    it("should change the current d3 event", function() {
+        var bars = [];
+
+        el.selectAll('.foo')
+          .data([1, 2, 3, 4])
+          .enter().append('div')
+              .attr('class', 'foo')
+              .attr('data-id', function(d) { return d; });
+
+        var oldEvent = d3.event = {baz: 'quux'};
+
+        el.selectAll('.foo')
+            .on('bar', function() { bars.push(d3.event); })
+            .simulate('bar', {corge: 'grault'});
+
+        expect(bars[0].type).to.equal('bar');
+        expect(bars[0].sourceEvent).to.equal(oldEvent);
+        expect(bars[0].target).to.equal(el.select('.foo[data-id="1"]').node());
+
+        expect(bars[1].type).to.equal('bar');
+        expect(bars[1].sourceEvent).to.equal(bars[0]);
+        expect(bars[1].target).to.equal(el.select('.foo[data-id="2"]').node());
+
+        expect(bars[2].type).to.equal('bar');
+        expect(bars[2].sourceEvent).to.equal(bars[1]);
+        expect(bars[2].target).to.equal(el.select('.foo[data-id="3"]').node());
+
+        expect(bars[3].type).to.equal('bar');
+        expect(bars[3].sourceEvent).to.equal(bars[2]);
+        expect(bars[3].target).to.equal(el.select('.foo[data-id="4"]').node());
+
+        expect(d3.event).to.equal(oldEvent);
+    });
 });
--- public/js/testutils/simulate.js
+++ public/js/testutils/simulate.js
@@ -1,3 +1,40 @@
 angular.module('vumigo.testutils').factory('simulate', [function () {
-    return {};
+    /**
+     * Mimics the way d3 sets `d3.event`:
+     * https://github.com/mbostock/d3/blob/master/src/event/event.js#L35-L46
+     */
+    function d3Event(target, type, data) {
+        return $.Event(type, angular.extend({}, data || {}, {
+            sourceEvent: d3.event,
+            target: target
+        }));
+    }
+
+    function simulate(type, data) {
+        var oldEvent = d3.event;
+
+        this.each(function(d, i) {
+            var listener = d3.select(this).on(type);
+            if (!listener) return;
+
+            d3.event = d3Event(this, type, data);
+            listener.call(this, d, i);
+        });
+
+        d3.event = oldEvent;
+        return this;
+    }
+
+    function enable() {
+        d3.selection.prototype.simulate = simulate;
+    }
+
+    function disable() {
+        delete d3.selection.prototype.simulate;
+    }
+
+    return {
+        enable: enable,
+        disable: disable
+    };
 }]);
