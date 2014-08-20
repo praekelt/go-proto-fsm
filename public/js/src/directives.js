5c06f99b35a812cc1d4dd714c45791b89dcdb445
Andrei Todorov <andrei@teamgeek.co.za>
2014-08-20 09:17:17 +0200

    Create component add modals

--- public/js/src/directives.js
+++ public/js/src/directives.js
@@ -5,6 +5,7 @@ var directives = angular.module('vumigo.directives', []);
  */
 directives.directive('goCampaignDesigner', [
     '$rootScope',
+    '$modal',
     'canvasBuilder',
     'dragBehavior',
     'componentHelper',
@@ -17,7 +18,7 @@ directives.directive('goCampaignDesigner', [
     'channelLayout',
     'connectionLayout',
     'controlPointComponent',
-    function ($rootScope, canvasBuilder, dragBehavior, componentHelper,
+    function ($rootScope, $modal, canvasBuilder, dragBehavior, componentHelper,
                    conversationComponent, channelComponent, routerComponent,
                    connectionComponent, conversationLayout, routerLayout,
                    channelLayout, connectionLayout, controlPointComponent) {
@@ -52,6 +53,144 @@ directives.directive('goCampaignDesigner', [
             $scope.selectedEndpointId = null;
             $scope.componentSelected = false;
             $scope.connectPressed = false;
+            $scope.newComponent = null;
+            $scope.addingComponent = false;
+
+            $scope.reset = function () {
+                $scope.selectedComponentId = null;
+                $scope.selectedEndpointId = null;
+                $scope.componentSelected = false;
+                $scope.connectPressed = false;
+                $scope.newComponent = null;
+                $scope.addingComponent = false;
+
+                $scope.refresh();
+            };
+
+            /**
+             * Open modal dialog and capture new conversation details.
+             */
+            $scope.addConversation = function () {
+
+                var add = function (data) {
+                    $scope.newComponent = {
+                        type: 'conversation',
+                        data: data
+                    };
+                };
+
+                var modalInstance = $modal.open({
+                    templateUrl: '/templates/conversation_add_modal.html',
+                    size: 'md',
+                    controller: ['$scope', '$modalInstance', 'rfc4122', function ($scope, $modalInstance, rfc4122) {
+                        $scope.data = {
+                            endpoints: [{uuid: rfc4122.v4(), name: "default"}]
+                        };
+
+                        $scope.ok = function () {
+                            add($scope.data);
+                            $modalInstance.close();
+                        };
+
+                        $scope.cancel = function () {
+                            $modalInstance.dismiss('cancel');
+                        };
+                    }]
+                });
+            };
+
+            /**
+             * Open modal dialog and capture new channel details.
+             */
+            $scope.addChannel = function () {
+
+                var add = function (data) {
+                    $scope.newComponent = {
+                        type: 'channel',
+                        data: data
+                    };
+                };
+
+                var modalInstance = $modal.open({
+                    templateUrl: '/templates/channel_add_modal.html',
+                    size: 'md',
+                    controller: ['$scope', '$modalInstance', 'rfc4122', function ($scope, $modalInstance, rfc4122) {
+                        $scope.data = {
+                            endpoints: [{uuid: rfc4122.v4(), name: "default"}],
+                            utilization: 0.5
+                        };
+
+                        $scope.ok = function () {
+                            add($scope.data);
+                            $modalInstance.close();
+                        };
+
+                        $scope.cancel = function () {
+                            $modalInstance.dismiss('cancel');
+                        };
+                    }]
+                });
+            };
+
+            /**
+             * Open modal dialog and capture new router details.
+             */
+            $scope.addRouter = function () {
+                var add = function (data) {
+                    $scope.newComponent = {
+                        type: 'router',
+                        data: data
+                    };
+                };
+
+                var modalInstance = $modal.open({
+                    templateUrl: '/templates/router_add_modal.html',
+                    size: 'md',
+                    controller: ['$scope', '$modalInstance', 'rfc4122', function ($scope, $modalInstance, rfc4122) {
+                        $scope.data = {
+                            channel_endpoints: [{uuid: rfc4122.v4(), name: "default"}],
+                            conversation_endpoints: [{uuid: rfc4122.v4(), name: "default"}]
+                        };
+
+                        $scope.ok = function () {
+                            add($scope.data);
+                            $modalInstance.close();
+                        };
+
+                        $scope.cancel = function () {
+                            $modalInstance.dismiss('cancel');
+                        };
+                    }]
+                });
+            };
+
+            /**
+             * Remove the selected component after prompting the user to confirm.
+             */
+            $scope.remove = function () {
+                if ($scope.selectedComponentId) {
+
+                    var removeComponent = function () {
+                        componentHelper.removeById($scope.data, $scope.selectedComponentId);
+                        $scope.reset();
+                    };
+
+                    var modalInstance = $modal.open({
+                        templateUrl: '/templates/confirm_modal.html',
+                        size: 'md',
+                        controller: ['$scope', '$modalInstance', function ($scope, $modalInstance) {
+                            $scope.yes = function () {
+                                removeComponent();
+                                $modalInstance.close();
+                            };
+
+                            $scope.no = function () {
+                                $modalInstance.dismiss('cancel');
+                            };
+                        }]
+                    });
+                }
+            };
 
             $scope.refresh = function () {
                 $rootScope.$emit('go:campaignDesignerRepaint');
@@ -80,14 +219,16 @@ directives.directive('goCampaignDesigner', [
                 // If there was a component selected, unselect it.
                 if (oldValue.id) {
                     var component = componentHelper.getById($scope.data, oldValue.id);
-                    var metadata = componentHelper.getMetadata(component.data);
-                    metadata.selected = false;
-
-                    // If the selected component had a selected endpoint, unselect it
-                    if (oldValue.endpointId) {
-                        var endpoint = componentHelper.getEndpointById(component, oldValue.endpointId);
-                        metadata = componentHelper.getMetadata(endpoint.data);
+                    if (component) {
+                        var metadata = componentHelper.getMetadata(component.data);
                         metadata.selected = false;
+
+                        // If the selected component had a selected endpoint, unselect it
+                        if (oldValue.endpointId) {
+                            var endpoint = componentHelper.getEndpointById(component, oldValue.endpointId);
+                            metadata = componentHelper.getMetadata(endpoint.data);
+                            metadata.selected = false;
+                        }
                     }
                 }
 
@@ -122,9 +263,21 @@ directives.directive('goCampaignDesigner', [
                 $scope.refresh();  // Repaint the canvas
             });
 
+            $scope.$watch('newComponent', function (value) {
+                if (value) {
+                    $scope.addingComponent = true;
+                } else {
+                    $scope.addingComponent = false;
+                }
+            });
+
             $rootScope.$on('go:campaignDesignerSelect', function (event, componentId, endpointId) {
                 $scope.selectedComponentId = componentId || null;
                 $scope.selectedEndpointId = endpointId || null;
+            });
+
+            $rootScope.$on('go:campaignDesignerRemove', function (event) {
+                $scope.remove();
             });
         }
 
@@ -235,7 +388,18 @@ directives.directive('goCampaignDesigner', [
                 });
             }
 
+            function drop(event, coordinates) {
+                if (scope.newComponent) {
+                    var x = coordinates[0];
+                    var y = coordinates[1];
+                    componentHelper.addComponent(scope.data, scope.newComponent, x, y);
+                    scope.newComponent = null;
+                    repaint();
+                }
+            }
+
             $rootScope.$on('go:campaignDesignerRepaint', repaint);
+            $rootScope.$on('go:campaignDesignerClick', drop);
         }
 
         return {
--- public/js/src/services/utils.js
+++ public/js/src/services/utils.js
@@ -89,8 +89,9 @@ angular.module('vumigo.services').factory('svgToolbox', [function () {
     };
 }]);
 
-angular.module('vumigo.services').factory('canvasBuilder', ['zoomBehavior', 'svgToolbox',
-    function (zoomBehavior, svgToolbox) {
+angular.module('vumigo.services').factory('canvasBuilder', [
+    '$rootScope', 'zoomBehavior', 'svgToolbox',
+    function ($rootScope, zoomBehavior, svgToolbox) {
         return function () {
             var width = 2048;  // Default canvas width
             var height = 2048;  // Default canvas height
@@ -127,11 +128,18 @@ angular.module('vumigo.services').factory('canvasBuilder', ['zoomBehavior', 'svg
                     .viewportElement(viewportElement)
                     .call();
 
-                container.on('mousedown', function () {
-                    d3.select(this).classed('dragging', true);
-                }).on('mouseup', function () {
-                    d3.select(this).classed('dragging', false);
-                }).call(zoom);
+                container
+                    .on('mousedown', function () {
+                        d3.select(this).classed('dragging', true);
+                    })
+                    .on('mouseup', function () {
+                        d3.select(this).classed('dragging', false);
+                        var coordinates = d3.mouse(this);
+                        $rootScope.$apply(function () {
+                            $rootScope.$emit('go:campaignDesignerClick', coordinates);
+                        });
+                    })
+                    .call(zoom);
 
                 return canvas;
             };
@@ -195,6 +203,36 @@ angular.module('vumigo.services').factory('componentHelper', ['$rootScope', 'rfc
             }
 
             return null;
+        };
+
+        function removeById(data, componentId) {
+            for (var i = 0; i < data.conversations.length; i++) {
+                if (data.conversations[i].uuid == componentId) {
+                    data.conversations.splice(i, 1);
+                    return;
+                }
+            }
+
+            for (var i = 0; i < data.channels.length; i++) {
+                if (data.channels[i].uuid == componentId) {
+                    data.channels.splice(i, 1);
+                    return;
+                }
+            }
+
+            for (var i = 0; i < data.routers.length; i++) {
+                if (data.routers[i].uuid == componentId) {
+                    data.routers.splice(i, 1);
+                    return;
+                }
+            }
+
+            for (var i = 0; i < data.routing_entries.length; i++) {
+                if (data.routing_entries[i].uuid == componentId) {
+                    data.routing_entries.splice(i, 1);
+                    return;
+                }
+            }
         };
 
         function getByEndpointId(data, endpointId) {
@@ -320,12 +358,36 @@ angular.module('vumigo.services').factory('componentHelper', ['$rootScope', 'rfc
             return component._meta;
         }
 
+        function addComponent(data, component, x, y) {
+            angular.extend(component.data, {
+                uuid: rfc4122.v4(),
+                x: x,
+                y: y
+            });
+
+            switch (component.type) {
+                case 'conversation':
+                    data.conversations.push(component.data);
+                    break;
+
+                case 'channel':
+                    data.channels.push(component.data);
+                    break;
+
+                case 'router':
+                    data.routers.push(component.data);
+                    break;
+            }
+        }
+
         return {
             getById: getById,
+            removeById: removeById,
             getByEndpointId: getByEndpointId,
             connectComponents: connectComponents,
             getEndpointById: getEndpointById,
-            getMetadata: getMetadata
+            getMetadata: getMetadata,
+            addComponent: addComponent
         };
     }
 ]);
