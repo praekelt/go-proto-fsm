7205ffb0de187e18ece5a4eacceb5dfe3ed618b7
Andrei Todorov <andrei@teamgeek.co.za>
2014-10-16 13:57:33 +0200

    Fix router tests

--- public/js/test/services/router/layout.js
+++ public/js/test/services/router/layout.js
@@ -1,30 +1,62 @@
 describe('routerLayout', function () {
-    var layout;
+    var data, manager, layout;
 
     beforeEach(module('vumigo.services'));
     beforeEach(module('uuid'));
 
-    beforeEach(inject(function (routerLayout) {
+    beforeEach(inject(function (ComponentManager, routerLayout) {
+        data = {
+            routing_table: {
+                version: 'fsm-0.1',
+                campaign_id: 'campaign1',
+                components: {
+                    'router1': {
+                        type: 'router',
+                        router_type: 'keyword',
+                        uuid: 'router1',
+                        name: 'A',
+                        description: 'Keyword',
+                        endpoints: {
+                            'endpoint1': {
+                                type: 'channel_endpoint',
+                                uuid: 'endpoint1',
+                                name: 'default'
+                            },
+                            'endpoint2': {
+                                type: 'conversation_endpoint',
+                                uuid: 'endpoint2',
+                                name: 'default'
+                            }
+                        }
+                    }
+                },
+                routing: {},
+            },
+            layout: {
+                version: 'fsm-ui-0.1',
+                components: {
+                    'router1': {
+                        x: 100,
+                        y: 100
+                    }
+                },
+                routing: {},
+                connections: {}
+            }
+        };
+
+        manager = new ComponentManager(data);
+
         layout = routerLayout();
     }));
 
     it('should compute router layout', inject(function (Router, Endpoint) {
-        var routers = [new Router({
-            id: "router1",
-            name: "A",
-            description: "Keyword",
-            endpoints: [
-                new Endpoint({ id: 'endpoint1', name: 'default', accepts: ['channel'] }),
-                new Endpoint({ id: 'endpoint2', name: 'default', accepts: ['conversation'] })
-            ],
-            x: 100,
-            y: 100
-        })];
+        var routers = manager.findComponents({ type: 'router' });
 
         layout(routers);
 
         var size = Math.max(layout.minSize(),
-            routers[0].getEndpoints('conversation').length * layout.pinGap());
+            routers[0].endpoints('conversation_endpoint').length * layout.pinGap());
 
         var radius = Math.sqrt(2.0 * Math.pow(size, 2)) / 2.0;
 
@@ -51,8 +83,8 @@ describe('routerLayout', function () {
         }];
 
         var actual = [
-            routers[0].endpoints[0].meta(),
-            routers[0].endpoints[1].meta()
+            routers[0].endpoints()[0].meta(),
+            routers[0].endpoints()[1].meta()
         ];
 
         expect(actual).to.deep.equal(expected);
